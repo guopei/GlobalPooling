286e04a6277a7785f08cc4ea7eb6a4e5864ba208
Pei Guo <peiguo@cs.byu.edu>
2019-08-05 16:02:34 -0600

    added new files

---
+++ simple.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=2 python main.py /mv_users/peiguo/dataset/cub-fewshot/full/ --nclasses 200 --gap GMP --pretrained  --print-freq 100 --lr 0.001 30 0.0001 20 #--resume checkpoints/2019_03_21_21_16_09_checkpoint.pth.tar -e
