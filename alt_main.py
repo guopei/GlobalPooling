rotation didn't work, spelling fix

alt_main.py
@@ -75,7 +75,7 @@ def main():
     torch.cuda.manual_seed_all(args.manualSeed)
     torch.manual_seed(args.manualSeed)
 
-    model = resnet152(pretrained=args.pretrained, num_classes=args.nclasses, pool_name=args.pool_name)
+    model = resnet50(pretrained=args.pretrained, num_classes=args.nclasses, pool_name=args.pool_name)
 
     print(model)
 

global_pool.py
@@ -68,7 +68,7 @@ class GlobalLogAvgExpPool2d(torch.nn.Module):
         b, c, h, w = x.size()
         x = x.view(-1, h*w)
         # equation from "a theoretical analysis of feature pooling ..."
-        # pytorch's implementation is umerically-stable I guess, see:
+        # pytorch's implementation is numerically-stable I guess, see:
         # https://www.xarg.org/2016/06/the-log-sum-exp-trick-in-machine-learning/ 
         # for reference.
         y = 1 / self.beta * (x.logsumexp(-1).view(b,c) - math.log(h*w))